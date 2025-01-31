24a10d6d35f6f945c1891abaf19871578a1f4268
nicolo_canevari <nico.canevari@outlook.it>
2025-01-31 14:37:32 +0100

    [FEAT] JS: funzione firstName lastName

---
+++ js/names.js
@@ -0,0 +1,12 @@
+// Funzione che restituisce un oggetto con firstName e lastName
+function createPerson(firstName, lastName) {
+
+  return {
+
+    firstName: firstName,
+    lastName: lastName
+
+  };
+
+}
+
