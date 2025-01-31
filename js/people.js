46a7e6d92c9f9358ef5944db03568308fb129838
nicolo_canevari <nico.canevari@outlook.it>
2025-01-31 15:46:42 +0100

    [FIX] refuso

--- js/people.js
+++ js/people.js
@@ -3,3 +3,23 @@ const createPerson = require('./names.js');
 
 // Importo la funzione da hobbies.js
 const createHobbies = require('./hobbies.js');
+
+// Creo una funzione senza parametri che restituisce un oggetto con fullname e hobbies
+function createFullProfile() {
+
+    // Uso le due funzioni precedentemente importate per costruire l'oggetto
+    const person = createPerson(`Alessandro`, `Magno`);
+    const hobbies = createHobbies(`Conquistare l'Impero Persiano`, `Costruire città`, `Scoprire nuove culture`);
+
+    // Restituisco l'oggetto con i dati
+    return {
+
+        fullName: `${person.firstName} ${person.lastName}`,
+        hobbies: hobbies
+
+    };
+
+}
+
+console.log(createFullProfile());
+
